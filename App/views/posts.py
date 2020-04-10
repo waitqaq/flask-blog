1b84bd2ca94b68fe5866764242c7a818aa823016
alone <1281141257@qq.com>
2020-04-10 23:05:54 +0800

    删除其他用户发文权限

--- App/views/posts.py
+++ App/views/posts.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, flash, redirect, url_for, current_
 from sqlalchemy import or_
 
 from App.forms import SendPosts, Comment, Catgs  # 导入发表博客与发表评论的表单类
-from App.models import Posts,Categorys  # 导入博客模型类和标签模型类
+from App.models import Posts,Categorys,User # 导入博客模型类和标签模型类
 from flask_login import current_user, login_required
 
 from datetime import datetime
@@ -36,34 +36,35 @@ def img_zoom(path, width=1000, height=250):
 @posts.route('/send_posts/', methods=['GET', 'POST'])
 def send_posts():
     form = SendPosts()
-
+    user = User.query.get(1)
     ctgs = Categorys.query.all()
     # 判断登录后在进行发表
     if not current_user.is_authenticated:
         flash('您还没有登录  请登录后再发表')
+    if current_user.id==1:
+        if form.validate_on_submit():
+            # md格式
+            article = request.form['article']
+            ctgs = request.form['ctgs']
+            tags = Categorys.query.filter_by(categorys=ctgs).first()
+            # 图片上传
+            img = request.files.get('img')  # 获取上传对象
+            suffix = img.filename.split('.')[-1]  # 获取后缀
+            newName = random_filename(suffix)  # 获取新图片的名称
+            # 以新名称保存图片
+            file.save(img, name=newName)
+            delPath = current_app.config['UPLOADED_PHOTOS_DEST']
+            # 拼接图片路径
+            path = os.path.join(delPath, newName)
+            # 进行缩放
+            img_zoom(path)
+            post = Posts(title=form.title.data, user=current_user, article=article, img=newName,tags=[tags])
+            post.save()
+            flash('博客发表成功')
+            return redirect(url_for('main.index'))
     else:
-        if current_user.id != 1:
-            flash('您没有发表博客的权限！')
-            if form.validate_on_submit():
-                # md格式
-                article = request.form['article']
-                ctgs = request.form['ctgs']
-                tags = Categorys.query.filter_by(categorys=ctgs).first()
-                # 图片上传
-                img = request.files.get('img')  # 获取上传对象
-                suffix = img.filename.split('.')[-1]  # 获取后缀
-                newName = random_filename(suffix)  # 获取新图片的名称
-                # 以新名称保存图片
-                file.save(img, name=newName)
-                delPath = current_app.config['UPLOADED_PHOTOS_DEST']
-                # 拼接图片路径
-                path = os.path.join(delPath, newName)
-                # 进行缩放
-                img_zoom(path)
-                post = Posts(title=form.title.data, user=current_user, article=article, img=newName,tags=[tags])
-                post.save()
-                flash('博客发表成功')
-                return redirect(url_for('main.index'))
+        flash('您没有发表博客的权限！')
+        return redirect(url_for('main.index'))
     return render_template('posts/send_posts.html', form=form, ctgs=ctgs)
 
 
